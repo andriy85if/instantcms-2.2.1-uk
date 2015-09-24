ed5f127d83a8ed4344c587921b65639d2fd87f98
andriy85if <work@andriy.bokiy.name>
2015-09-24 14:05:31 +0300

    adding system localization

---
+++ system/languages/uk/controllers/content/widgets/slider.php
@@ -0,0 +1,7 @@
+<?php
+
+    define('LANG_WD_CONTENT_SLIDER_DATASET',  'Назва набору');
+    define('LANG_WD_CONTENT_SLIDER_IMAGE',    'Поле зображення');
+    define('LANG_WD_CONTENT_SLIDER_TEASER',   'Поле короткого опису');
+    define('LANG_WD_CONTENT_SLIDER_DELAY',    'Затримка між слайдами');
+    define('LANG_WD_CONTENT_SLIDER_DELAY_HINT',    'Введіть нуль, щоб відключити автоматичну зміну слайдів');
